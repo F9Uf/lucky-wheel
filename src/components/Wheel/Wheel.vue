5f0b29054ceb08c046f6d987ec31d6af49f2a106
F9Uf <atoseka@gmail.com>
2022-09-27 23:02:09 +0700

    fix: wheel alignment

--- src/components/Wheel/Wheel.vue
+++ src/components/Wheel/Wheel.vue
@@ -4,14 +4,26 @@
       <div
         v-for="(item, index) in items"
         :key="index"
-        :class="`item absolute left-1/2 ${calculateItemBg(index)}`"
-        :style="{'transform': calculateItemTransform(index)}"
+        :class="`
+          overflow-hidden absolute w-[50%] h-[50%]
+          top-0 right-0
+          ${calculateItemBg(index)}
+        `"
+        :style="{
+          transform: `
+            rotate(${degreePerItem * index}deg)
+            skewY(${-(90 - degreePerItem)}deg)
+          `,
+          transformOrigin: '0% 100%'
+        }"
       >
         <div
-          class="absolute w-[10%] h-[50%] left-[60px] text-center"
-          :style="{'transform': calculateItemContentTransform(index)}"
+          class="absolute -left-full w-[200%] h-[200%] text-center pt-5"
+          :style="{
+            transform: `skewY(${90 - degreePerItem}deg) rotate(${degreePerItem / 2}deg)`
+          }"
         >
-          {{ item }}
+          <span>{{ item }}</span>
         </div>
       </div>
     </div>
@@ -41,12 +53,6 @@ export default defineComponent({
     const positionCenterStyle = computed((): string => "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2")
     const degreePerItem = computed((): number => 360 / props.items.length)
 
-    const calculateItemTransform = (index: number): string => {
-      const degree = degreePerItem.value * index
-
-      return `rotate(${degree}deg) skewY(${-(90 - degreePerItem.value)}deg)`
-    }
-
     const calculateItemBg = (index: number): string => {
       const bgColors = [
         'bg-violet-200', 'bg-violet-600', 'bg-violet-400', 'bg-violet-800', 'bg-violet-500', 'bg-violet-700'
@@ -57,32 +63,11 @@ export default defineComponent({
       return bgColors[bgIndex]
     }
 
-    const calculateItemContentTransform = (index: number): string => {
-      return `skewY(${90 - degreePerItem.value}deg) rotate(295deg)`
-    }
-
     return {
       positionCenterStyle,
       degreePerItem,
-      calculateItemTransform,
       calculateItemBg,
-      calculateItemContentTransform,
     }
   }
 })
 </script>
-
-<style scoped>
-.item {
-  height: 50%;
-	width: 50%;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	font-size: 20px;
-	font-family: monospace;
-	font-weight: 1000;
-	writing-mode: vertical-rl;
-  transform-origin: 0% 100%;
-}
-</style>
